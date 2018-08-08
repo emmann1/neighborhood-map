2702c223508da69dca9c10690dde8d4b3f4b9109
Emanuel Mares <emanuel.mares@gmail.com>
2018-08-08 21:18:47 +0300

    Added markers to map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from  './Components/home';
+import InfoWindow from  './Components/infoWindow';
 import * as markedLocations from './locations.json';
 
 class App extends Component {
@@ -25,10 +26,30 @@ class App extends Component {
     
   }
 
+  //initialize the map and markers
   initMap= () => {
+    let locations = this.state.locations;
+    let markers = this.state.markers;
     let map = new window.google.maps.Map(document.getElementById('map'), {
-      zoom: 15,
-      center: { lat: 45.756605, lng: 21.229135 }
+      zoom: 16,
+      center: { lat: 45.755374, lng: 21.228449 }
+    });
+
+    //Loops through every location to set a marker for it
+    locations.forEach( element => {
+      let position = element.position;
+      let name = element.title;
+      let id = element.key;
+
+      let marker = new window.google.maps.Marker({
+        map: map,
+        position: position,
+        title: name,
+        id: id,
+        animation: window.google.maps.Animation.DROP
+      });
+
+      markers.push(marker);
     });
 
     /* Keep state in sync */
---
+++ src/Components/infoWindow.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class InfoWindow extends Component {
+
+
+}
+
+export default InfoWindow
