f486136e92dd7b8357f29ab1c87902ee41648560
Emanuel Mares <emanuel.mares@gmail.com>
2018-08-08 20:21:24 +0300

    Created locations json file and imported it in APP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from  './Components/home';
+import * as markedLocations from './locations.json';
 
 class App extends Component {
+
+  state = {
+    locations: markedLocations,
+    map: '',
+    markers: [],
+    currentMarker: ''
+  }
+
+  //Load the map API and setting the global initMap as this components initMap
+  componentDidMount() {
+    window.initMap = this.initMap;
+    Load('https://maps.googleapis.com/maps/api/js?key=AIzaSyBV0VFuM6WqVpdMx071AkSNbViOjeerMYI&callback=initMap');
+  }
+
+  initMap() {
+
+  }
+
   render() {
     return (
       <Home />
